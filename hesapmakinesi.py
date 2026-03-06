e01eee8fa6e836389c7cbf81a20c6804ba502158
elfaktas <elifaktas00@gmail.com>
2026-03-07 01:02:54 +0300

    kontrol

--- hesapmakinesi.py
+++ hesapmakinesi.py
@@ -1,11 +1,17 @@
 print()
 print("\033[1;32;40m Renkli yazı  \n")
 print(" ╔═════════════════════╗")
-print(" ║    ÇİZİMLER         ║")
+print(" ║    HESAP MAKİNESİ   ║")
 print(" ╠═════════════════════╣")
-print(" ║  1-Kare             ║")
-print(" ║  2-Üçgen            ║")
-print(" ║  3-Desen            ║")
-print(" ║  4-                 ║")
+print(" ║  1-Toplama          ║")
+print(" ║  2-Çarpma           ║")
+print(" ║  3-                 ║")
+print(" ║  4-Çıkış            ║")
 print(" ║   Seçiminiz?        ║")
 print(" ╚═════════════════════╝")
+
+ secim=input("Seçiminiz nedir?")
+if seçim in ["1","2","3"]:
+     print(secim,"sectiniz")
+     print("Hesaplamalar bölümüne yönlendirileceksiniz")
+     import moduller1.hesapmakinesi  
