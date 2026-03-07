21d2a0fdf416404aa941e4e7e392de739bb6f656
elfaktas <elfaktas00@gmail.com>
2026-03-07 10:47:08 +0300

    kontrol

---
+++ anaprogram.py
@@ -0,0 +1,31 @@
+# vectorelapp_proje1
+
+print ()
+print ("\033[1;32;40m Renkli yazı\n")
+print (" ╔═════════════════════╗ ")
+print (" ║    VEKTOREL APP     ║ ")
+print (" ╠═════════════════════╣ ")
+print (" ║  1-Hesaplamalar     ║ ")
+print (" ║  2-Oyunlar          ║ ")
+print (" ║  3-Çizimler         ║ ")
+print (" ║  4-                 ║ ")
+print (" ║  5-                 ║ ")
+print (" ║  6-                 ║ ")
+print (" ║  7-                 ║ ")
+print (" ║  8-                 ║ ")
+print (" ║  9-                 ║ ")
+print (" ║  C-Çıkış            ║ ")
+print (" ║                     ║ ")
+print (" ╚═════════════════════╝ ")
+
+secim = input("Seçiminiz nedir?")
+if secim in ["1","2","3"]:
+    if secim == "1" : 
+        print(secim, "sectiniz")
+        print("Hesaplamalar bölümüne yönlendirileceksiniz")
+    import moduller1.hesapmakinesi
+    if secim == "2" : 
+        print(secim, "sectiniz")
+        print("Oyunlar bölümüne yönlendirileceksiniz")
+else :
+        print("Böyle bir seçenek yok.")
