85809cbc780afefd4fdf19edda08b5ca0d992867
elfaktas <elifaktas00@gmail.com>
2026-03-22 19:09:45 +0300

    kontrol

--- anaprogram.py
+++ anaprogram.py
@@ -1,4 +1,9 @@
 # vectorelapp_proje1
+#main_menu
+import moduller1.hesap_makinesi
+import moduller1.çizimler2
+import moduller1.oyunlar3
+
 
 print ()
 print ("\033[1;32;40m Renkli yazı\n")
@@ -12,14 +17,21 @@ print (" ║  C-Çıkış            ║ ")
 print (" ║                     ║ ")
 print (" ╚═════════════════════╝ ")
 
-secim = input("Seçiminiz nedir?")
-if secim in ["1","2","3"]:
-    if secim == "1" : 
-        print(secim, "sectiniz")
-        print("Hesaplamalar bölümüne yönlendirileceksiniz")
-    import moduller1.hesapmakinesi
-    if secim == "2" : 
-        print(secim, "sectiniz")
-        print("Oyunlar bölümüne yönlendirileceksiniz")
-else :
-        print("Böyle bir seçenek yok.")
+try:
+        a = int(input("Lütfen bir işlem seçiniz:\t"))
+        if a == 1:
+            print(f"{a}'e bastınız; Hesaplamalar bölümüne yönlendiriliyorsunuz.\n\n")
+            moduller1.hesap_makinesi.calistir()
+        elif a == 2:
+            print(f"{a}'ye bastınız; Şekil Çizdirme bölümüne yönlendiriliyorsunuz.\n\n")
+            moduller1.sekil_cizdirme2.calistir()
+        elif a == 3:
+            print(f"{a}'e bastınız; Oyunlar bölümüne yönlendiriliyorsunuz.\n\n")
+            moduller1.oyunlar3.calistir()
+        elif a == 0:
+            print('Programdan çıkılıyor...')
+            break
+        else:
+            print("Lütfen Hesap Makinesinde belirtilen işlemlerden birini seçiniz!\n"*3)
+    except ValueError:
+        print("Hata: Lütfen sayı giriniz!")
